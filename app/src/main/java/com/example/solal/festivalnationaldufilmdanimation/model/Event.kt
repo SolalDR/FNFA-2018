2a460113769ff4d92525cdacde085323d2a6f263
Solal Dussout-Revel <solal.dussout-revel@hotmail.fr>
2017-12-11 17:05:26 +0100

    Add property to Event

--- app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/model/Event.kt
+++ app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/model/Event.kt
@@ -1,7 +1,26 @@
 package com.example.solal.festivalnationaldufilmdanimation.model
 
+import org.json.JSONObject
+import java.util.Date
+
 /**
  * Created by sdussoutrevel on 11/12/2017.
  */
-class Event {
+class Event constructor(
+        jsonObject: JSONObject
+){
+    init {
+        val id: Int
+        val name: String
+        val description_short: String
+        val description: String
+        val date_start: Date
+        val date_end: Date
+        val duration: Int //Seconds
+        val background: String
+        val type: Int
+        val age_min: Int  // Ex: 6 => 6 years old or more
+        val author: Author
+        val scene: Scene
+    }
 }
