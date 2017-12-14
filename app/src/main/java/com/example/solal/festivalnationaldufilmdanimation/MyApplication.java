8b6ac5eed0bf4349cc58dcfc2e0cf23e6c4977a3
Solal Dussout-Revel <solal.dussout-revel@hotmail.fr>
2017-12-14 14:08:31 +0100

    Update json replace 2017 -> 2018 & refactor MyApplication

--- app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/MyApplication.java
+++ app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/MyApplication.java
@@ -29,18 +29,7 @@ public class MyApplication extends Application  {
     public void onCreate() {
         super.onCreate();
 
-        Moshi moshi = new Moshi.Builder().build();
-
-        String scenesRaw = JsonParser.getStringFromJson(R.raw.scenes, this.getApplicationContext());
-
         this.dataManager = new DataManager(this.getApplicationContext());
-
-
-        //List<Scene> scenes = this.loadScenes(moshi);
-        //if(!scenes.isEmpty()) {
-        //    this.dataManager.setScenes(new ArrayList<Scene>(this.loadScenes(moshi)));
-        //}
-
         this.dataManager.launchData();
         ArrayList<Scene> scenes = this.dataManager.findAllScenes();
         for (int counter = 0; counter < scenes.size(); counter++) {
@@ -48,61 +37,7 @@ public class MyApplication extends Application  {
         }
     }
 
-    public void onActivityResumed(){
-        this.dataManager = new DataManager(this.getApplicationContext());
-        this.dataManager.launchData();
-    }
-
     private DataManager getDataManager(){
         return this.dataManager;
     }
-
-    private List<EventType> loadEventsType(Moshi moshi) {
-
-        String eventsTypeRaw = JsonParser.getStringFromJson(R.raw.events_type, this.getApplicationContext());
-
-        Type type = Types.newParameterizedType(List.class, EventType.class);
-        JsonAdapter<List<EventType>> adapter = moshi.adapter(type);
-
-        List<EventType> eventsTypes = null;
-        try {
-            eventsTypes = adapter.fromJson(eventsTypeRaw);
-        } catch(IOException e) {
-            System.out.println("There is no scenes");
-        }
-        return eventsTypes;
-    }
-
-    private List<Event> loadEvents(Moshi moshi) {
-
-        String eventsRaw = JsonParser.getStringFromJson(R.raw.events, this.getApplicationContext());
-
-        Type type = Types.newParameterizedType(List.class, Event.class);
-        JsonAdapter<List<Event>> adapter = moshi.adapter(type);
-
-        List<Event> events = null;
-        try {
-            events = adapter.fromJson(eventsRaw);
-        } catch(IOException e) {
-            System.out.println("There is no events");
-        }
-        return events;
-    }
-
-    private List<Scene> loadScenes(Moshi moshi) {
-
-        String scenesRaw = JsonParser.getStringFromJson(R.raw.scenes, this.getApplicationContext());
-
-        Type type = Types.newParameterizedType(List.class, Scene.class);
-        JsonAdapter<List<Scene>> adapter = moshi.adapter(type);
-
-        List<Scene> scenes = null;
-        try {
-            scenes = adapter.fromJson(scenesRaw);
-        } catch(IOException e) {
-            System.out.println("There is no scenes");
-        }
-        return scenes;
-    }
-
 }
--- app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/entity/Place.kt
+++ app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/entity/Place.kt
@@ -1,6 +1,7 @@
 package com.example.solal.festivalnationaldufilmdanimation.entity
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -14,21 +15,27 @@ class Place constructor(
     var lat: Float? = null
     var lon: Float? = null
     var address: String? = null
-    //var date_start: Date
-    //var date_end: Date
+    var date_start: Date
+    var date_end: Date
 
     init {
         jsonObject.getString("lat")?.apply { lat = this.toFloat() }
         jsonObject.getString("lon")?.apply { lon = this.toFloat() }
         jsonObject.getString("address")?.apply { address = this }
+        val sdfmt1 = SimpleDateFormat("yyyy-MM-dd HH:mm:SS", Locale.getDefault())
+        date_start = sdfmt1.parse(jsonObject.getString("date_start"))
+        date_end = sdfmt1.parse(jsonObject.getString("date_end"))
+
     }
 
     fun inspect(indent:String = ""){
-        System.out.println(indent + "-----------PLACE-----------");
-        System.out.println(indent + "Place");
-        System.out.println(indent + "lat : "+ lat);
-        System.out.println(indent + "lon : "+ lon);
-        System.out.println(indent + "address : "+ address);
-        System.out.println(indent + "-----------/PLACE-----------");
+        System.out.println(indent + "-----------PLACE-----------")
+        System.out.println(indent + "Place")
+        System.out.println(indent + "lat : "+ lat)
+        System.out.println(indent + "lon : "+ lon)
+        System.out.println(indent + "address : "+ address)
+        System.out.println(indent + "date start : "+ date_start)
+        System.out.println(indent + "date end : "+ date_end)
+        System.out.println(indent + "-----------/PLACE-----------")
     }
 }
