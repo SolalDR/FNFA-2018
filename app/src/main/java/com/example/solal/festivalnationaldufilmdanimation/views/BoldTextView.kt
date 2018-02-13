c94000ff6f66ee8ad38091b69a282663209b8d69
SolalDR <solal.dussout-revel@hotmail.fr>
2018-02-13 17:37:35 +0100

    Refactor all style

---
+++ app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/views/BoldTextView.kt
@@ -0,0 +1,39 @@
+
+package com.example.solal.festivalnationaldufilmdanimation.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.support.v7.widget.AppCompatTextView
+
+/**
+ * Created by sdussoutrevel on 13/02/2018.
+ */
+
+
+class BoldTextView : AppCompatTextView {
+
+
+    constructor(context: Context) : super(context) {
+        val face = Typeface.createFromAsset(context.assets, "font/Montserrat-Bold.ttf")
+        this.typeface = face
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val face = Typeface.createFromAsset(context.assets, "font/Montserrat-Bold.ttf")
+        this.typeface = face
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        val face = Typeface.createFromAsset(context.assets, "font/Montserrat-Bold.ttf")
+        this.typeface = face
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+
+    }
+
+}
