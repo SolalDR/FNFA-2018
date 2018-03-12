03bab63af30755796cdb0e11f9d48933a8e1821e
SolalDR <solal.dussout-revel@hotmail.fr>
2018-03-12 18:34:35 +0100

    Improve FavoriteActivity layout

--- app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/MainActivity.kt
+++ app/src/main/java/com/example/solal/festivalnationaldufilmdanimation/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.BitmapFactory
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v4.app.NotificationCompat
 import android.view.View
 import android.widget.ImageButton
@@ -46,33 +46,14 @@ abstract class MainActivity : AppCompatActivity() {
     fun sendNotification(view: View) {
 
         //Get an instance of NotificationManager//
-//        val notification=notificationBuilder.buildild()
         val mBuilder = NotificationCompat.Builder(this)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("My notification")
                 .setContentText("Hello World!")
 
-
-
         // Gets an instance of the NotificationManager service//
-
-        val mNotificationManager =
-
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // When you issue multiple notifications about the same type of event,
-        // it’s best practice for your app to try to update an existing notification
-        // with this new information, rather than immediately creating a new notification.
-        // If you want to update this notification at a later date, you need to assign it an ID.
-        // You can then use this ID whenever you issue a subsequent notification.
-        // If the previous notification is still visible, the system will update this existing notification,
-        // rather than create a new one. In this example, the notification’s ID is 001//
-
-//        NotificationManager.notify().mNotificationManager.notify(1, mBuilder.build())
-
-//        NotificationManager.n
-
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         mNotificationManager.notify(1, mBuilder.build());
 
